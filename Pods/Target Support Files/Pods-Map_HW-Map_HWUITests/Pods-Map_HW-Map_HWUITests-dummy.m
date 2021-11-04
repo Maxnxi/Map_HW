8b4396a6e6ed9da4263778990b07ff391b0799e8
Maxnxi <nisq@ya.ru>
2021-11-04 20:27:50 +0300

    deleting Pods

--- Pods/Target Support Files/Pods-Map_HW-Map_HWUITests/Pods-Map_HW-Map_HWUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Map_HW_Map_HWUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Map_HW_Map_HWUITests
-@end
--- Pods/Target Support Files/Pods-Map_HWTests/Pods-Map_HWTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Map_HWTests : NSObject
-@end
-@implementation PodsDummy_Pods_Map_HWTests
-@end
