2ba1a3b08c3e7e442fc2e56c4ce3f096c65025e1
Maxnxi <nisq@ya.ru>
2021-11-13 11:50:07 +0300

    .

---
+++ Map_HW/Flow/Auth/RegisterViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RegisterViewController.swift
+//  Map_HW
+//
+//  Created by Maksim Ponomarev on 12.11.2021.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    static let reuseIdentifier = "registerViewController"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
