e14ca7810e0efe7e4e3c844510a7a82a7fcac86e
Maxnxi <nisq@ya.ru>
2021-11-15 14:23:31 +0300

    добавлено ДЗ №4
    однако - шторка не убирается

--- Map_HW/SceneDelegate.swift
+++ Map_HW/SceneDelegate.swift
@@ -10,6 +10,7 @@ import GoogleMaps
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
+    var coverView: UIView?
     var window: UIWindow?
     var coordinator: ApplicationCoordinator?
 
@@ -19,7 +20,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         GMSServices.provideAPIKey("AIzaSyAuW-cBcK81Fq22yEn92y_fdBeGL8n6qq0")
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        //windowScene.sizeRestrictions = UISceneSizeRestrictions().maximumSize
         window = UIWindow(windowScene: windowScene)
+//        window?.bounds.size = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        //window?.bounds.size = CGSize(width: 400 , height: 1500)
         window?.makeKeyAndVisible()
         window?.sizeToFit()
         window?.clipsToBounds = true
@@ -39,22 +43,47 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        print(#function)
+        //coverView = nil
+        //coverView?.removeFromSuperview()
+//        let rect = CGRect(x: 0, y: 0, width: 0, height: 0)
+//        coverView = UIView(frame: rect)
+//        self.window?.addSubview(coverView!)
+        guard let view = coverView else { return }
+        window?.willRemoveSubview(view)
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
+        print(#function)
+        let screenSize = UIScreen.main.bounds.size
+        let rect = CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height)
+        coverView = UIView(frame: rect)
+        coverView?.backgroundColor = UIColor(displayP3Red: 1, green: 0.5, blue: 0.5, alpha: 1)
+        //coverView.backgroundColor.saturation(0.5)
+        self.window?.addSubview(coverView!)
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
+        //coverView = nil
+        print(#function)
+        coverView?.removeFromSuperview()
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+        print(#function)
+        let screenSize = UIScreen.main.bounds.size
+        let rect = CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height)
+        coverView = UIView(frame: rect)
+        coverView?.backgroundColor = UIColor(displayP3Red: 1, green: 0.5, blue: 0.5, alpha: 1)
+        self.window?.addSubview(coverView!)
+        //coverView.backgroundColor.saturation(0.5)
     }
 
 
